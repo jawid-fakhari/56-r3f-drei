fa5d6a668bbc43eb293e767776a03f0faaffb60f
jawid-fakhari <jawid_fakhari@live.it>
2024-10-08 17:13:18 +0200

    add orbitcontrols

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,8 @@
+import { OrbitControls } from "@react-three/drei"
 
 export default function Experience() {
     return <>
-
+        <OrbitControls />
 
         <directionalLight position={[1, 2, 3]} intensity={4.5} />
         <ambientLight intensity={1.5} />
